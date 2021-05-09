3f61392753618afd0bff6140c40750e8b389e3f2
Udara Sachinthana <U4dara@gmail.com>
2021-05-10 00:01:48 +0530

    new component added

--- PizzaHub_React/src/index.js
+++ PizzaHub_React/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';// document- containe with the all element in t
 import {BrowserRouter, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from './components/navigation';
+import selectPizza from "./components/selectPizza";
 //import ViewItem from './components/admin_viewItem'
 //import Home from './components/admin_home'
 
 
-ReactDOM.render(<Navigation/>,document.getElementById("root"))
+ReactDOM.render(
+    <BrowserRouter>
+        <Navigation/>
+        <Route path="/pizzahub" component={home}/>
+        <Route exact path="/pizzahub/:id" component={selectPizza}/>
+    </BrowserRouter>
+,document.getElementById("root"))
 //ReactDOM.render(<ViewItem/>,document.getElementById("root"))
 //ReactDOM.render(<Home/>,document.getElementById("root"))
 
