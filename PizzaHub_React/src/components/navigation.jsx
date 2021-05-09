c3e1cdf9e0133659024faf5933e6dc771a6d821a
Udara Sachinthana <U4dara@gmail.com>
2021-05-09 20:41:37 +0530

    axios installed

--- PizzaHub_React/src/components/navigation.jsx
+++ PizzaHub_React/src/components/navigation.jsx
@@ -27,16 +27,16 @@ class Navigation extends Component{
                 <li className="nav-item ">
                   <a to="#" className="nav-link"  id="navbarDropdown"> ㅤContact Us ㅤ ㅤ</a>
                 </li>
-               </ul>
+              </ul>
               <form className="form-inline my-2 my-lg-4">
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
               </form>
               <ul className="nav navbar-nav navbar-right">
-                   <li><a to="#"><span className="glyphicon glyphicon-user" ></span>ㅤㅤSign Upㅤ</a></li>
-                   <span className="border border-primary"></span>
+                  <li><a to="#"><span className="glyphicon glyphicon-user" ></span>ㅤㅤSign Upㅤ</a></li>
+                  <span className="border border-primary"></span>
                     <li><a to="#"><span className="glyphicon glyphicon-log-in"></span> ㅤLogin</a></li>
-             </ul>
+            </ul>
             </div>
           </nav>
           <br/>
@@ -45,8 +45,8 @@ class Navigation extends Component{
             <Home/>
           </div>
           <div class="footer" style = {{position: 'fixed'}}  style = {{width: "40rem"}} 
-           style ={{backgroundColor: 'gray'}} >
-             <p>Footer</p>
+          style ={{backgroundColor: 'gray'}} >
+            <p>Footer</p>
           </div>
           </div>
           
