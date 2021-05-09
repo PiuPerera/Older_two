8d3bb957198ead8261b4726023b0d3d81debb482
Udara Sachinthana <U4dara@gmail.com>
2021-05-09 14:16:43 +0530

    initial commit

---
+++ PizzaHub_API/models/pizza.js
@@ -0,0 +1,36 @@
+const moongose = require("mongoose");
+
+const pizzaSchema = new moongose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 100
+    },
+    ingredients: [String],
+    size: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    availability: {
+        type: Boolean,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    details: {
+        type: String,
+        required: true
+    }
+});
+
+const Pizza = moongose.model("PizzaHub", pizzaSchema);
+
+module.exports = Pizza;
---
+++ PizzaHub_API/routes/pizzas.js
@@ -0,0 +1,91 @@
+const { request } = require("express");
+const express = require("express");
+const Pizza = require("../models/pizza");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try{
+        let pizzas = await Pizza.find()
+        return res.send(pizzas);
+    }
+    catch(err){
+        return res.send("Error :", err.message).status(500);
+    }
+    
+});
+
+router.get("/:id", (req, res) => {
+    let requestID = req.params.id;
+    let pizza = Pizza.find();
+    if(!pizza)
+    {
+        return res.send("The pizza you looking for dosen't available").status(404);
+    }
+    return res.send(pizza).status(200);
+});
+
+router.put("/:id", async (req, res) => {
+    let requestID = req.params.id;
+    let pizza = await Pizza.findById(requestID);
+    if(!pizza)
+    {
+        return res.send("The pizza you looking for dosen't available").status(404);
+    }
+    try{
+        pizza.set({
+            size: req.body.size,
+            price: req.body.price,
+            availability: req.body.availability
+        });
+        pizza = await (await pizza).save();
+        return res.send(pizza).status(200);
+    }
+    catch(err){
+        return res.send(err.message).status(500);
+    }
+    
+});
+
+router.post("/", async (req, res) => {
+    if(!req.body.name){
+        return res.send("Bad request").status(400);
+    }
+    
+    newPizza = new Pizza({
+        name: req.body.name,
+        ingredients: req.body.ingredients,
+        size: req.body.size,
+        price: req.body.price,
+        availability: req.body.availability,
+        imageUrl: req.body.imageUrl,
+        details: req.body.details
+    });
+    try{
+        newPizza = await newPizza.save();
+        return res.send(newPizza).status(200);
+    }
+    catch(err){
+        return res.send(err.message).status(500);
+    }
+    
+});
+
+router.delete("/:id", async (req, res) => {
+    let requestID = req.params.id;
+    let pizza = PizzaArray.find(pizza => pizza.id == requestID);
+    if(!pizza)
+    {
+        return res.send("The pizza you looking for dosen't available").status(404);
+    }
+    try{
+
+    }
+    catch(err){
+        
+    }
+    let indexOfPizza = PizzaArray.indexOf(pizza);
+    PizzaArray.splice(indexOfPizza);
+    return res.send(pizza).status(200);
+});
+
+module.exports = router;
