6480371ae46c6a2c0770053619ef3906adc04438
Piumali Perera <sachini95gsp@gmail.com>
2021-05-09 20:08:32 +0530

    NEW CHANGES

--- PizzaHub_React/src/index.js
+++ PizzaHub_React/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';// document- containe with the all element in t
 import {BrowserRouter, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from './components/navigation';
-import ViewItem from './components/admin_viewItem'
-import Home from './components/admin_home'
+//import ViewItem from './components/admin_viewItem'
+//import Home from './components/admin_home'
 
 
 ReactDOM.render(<Navigation/>,document.getElementById("root"))
